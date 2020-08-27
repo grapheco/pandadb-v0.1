cd380ffa682881faaf68ae733c76777524c4e0bc
MaseratiD <1140727287@qq.com>
2020-08-27 21:53:36 +0800

    neo4j-start bat scripts

--- packaging/src/main/distribution/bin/neo4j-start.bat
+++ packaging/src/main/distribution/bin/neo4j-start.bat
@@ -6,7 +6,7 @@ setlocal
   echo Directory=%Directory%
   for %%d in (%~dp0..) do set ParentDirectory=%%~fd
   echo ParentDirectory=%ParentDirectory%
-  set class_path=%ParentDirectory%\lib\pandadb-server-all-in-one-0.1.0-SNAPSHOT.jar
+  set class_path=%ParentDirectory%\lib\pandadb-dist-0.1.0-SNAPSHOT.jar
   set MainClass="cn.pandadb.server.PandaServerStarter"
   set config=%ParentDirectory%\conf\neo4j.conf
   set log_path=%ParentDirectory%\logs\neo4j.log
